bb6e8ecea32884e2ffcb23b9e0fd69cb55117c59
Tanay Padar <72204355+JuniorProgrammer30104@users.noreply.github.com>
2021-03-16 23:12:22 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,10 +2,9 @@
 #Youtube Channel Link -: https://tinyurl.com/1smu75d8
 #Youtube Channel Name -: Junior Programmer 
 
-
 #Install Modules 
 import pyttsx3 
-import speech_recognition
+import speech_recognition as sr
 import datetime
 import os
 import cv2
@@ -15,16 +14,15 @@ import webbrowser
 import smtplib
 import sys
 
-
+#Import Deafult JARVIS's Voice from PC
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
 
-
 def speak(audio):
     engine.say(audio)
-    print (audio)
+    print(audio)
     engine.runAndWait()
 
 def takecommand():
@@ -32,14 +30,14 @@ def takecommand():
     with sr.Microphone() as source:
         print ("Listening....")
         r.pause_threshold = 1
-        audio = r.listen(source,timeout=1 , phrase_time_limit =5)
+        audio = r.listen(source)
     try:
         print("Just a moment....")
         query=r.recognize_google(audio , language = 'en-in')
-        print (f"You said:{query}")
+        print (f"You said:{query}\n")
     except Exception as e :
         speak("Will you please repeat that....")
-        return "none"
+        return "None"
     return query
 
 def wish():
@@ -50,8 +48,8 @@ def wish():
         speak ("Good Afternoon Sir!")
     else :
         speak ("Good Evening!")
-    speak ("Hello , i am JARVIS made by Junior Programer. Please tell me how can i help you ?")
- 
+    speak ("Hello , i am JARVIS made by Junior Programmer. Please tell me how can i help you ?")
+
 #To send Email
 def sendEmail(to,content):
     server = smtplib.SMTP('smtp.gmail.com',587)
@@ -61,46 +59,34 @@ def sendEmail(to,content):
     server.sendmail('Your email id' , to ,content)
     server.close()
 
-
 if __name__ == "__main__":
     wish()
-    if 1:
+    while True:
+    #while 1:
         query = takecommand().lower()
-#Open Notepad 
-        if "open notepad" in query:
-            npath = "C:\\Users\\Manisha\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Accessories"#App Location 
-            os.startfile(npath)
 
 #Open OBS Studio
-         elif "open OBS Studio" in query:
-             opath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OBS Studio"
-             os.startfile(opath)
-             
+        if "open studio" in query:
+            opath = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\OBS Studio"#App Location 
+            os.startfile(opath)
+
+#Open Notepad 
+        if "open notepad" in query:
+            npath = "C:\\Users\\Manisha\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Accessories\\Notepad.Ink"#App Location 
+            os.startfile(npath)
+
 #Open CMD
-        elif "open command prompt" in query:
-            cpath = "C:\\Users\\Manisha\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\System Tools"#App Location
-            os.startfile(cpath)
+        elif "open cmd" in query:
+            os.system("start cmd")
 
 #Open VSC
         elif "open visual studio code" in query:
             vpath = "C:\\Users\\Manisha\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio Code" #App Location
             os.startfile(vpath)
 
-#Open Camera
-        elif "open camera" in query :
-            cap = cv2.VideoCapture(0)
-            while True:
-                ret , img = cap.read()
-                cv2.inshow('webcam',img)
-                k = cv2.waitKey(50)
-                if k== 27:
-                    break:
-            cap.release()
-            cv2.destroyAllWindows()
-
 #Play Music
         elif "play music" in query:
-            music_dir = "C:\\Users\\Manisha\\Downloads"
+            music_dir = "C:\\Users\\Manisha\\Downloads\\Song.mp3"
             songs = os.listdir(music_dir)
             os.startfile(os.path,join(music_dir,songs[0]))
 
@@ -119,47 +105,47 @@ if __name__ == "__main__":
             print (results)
 
 #Open Youtube
-         elif "Open Youtube" in query:
-             speak ("Sir, What should i search on Youtube ?")
-             cm=takecommand().lower()
-             webbrowser.open(f"{cm}")
+        elif "open youtube" in query:
+            speak ("Sir, What should i search on Youtube ?")
+            cm=takecommand().lower()
+            webbrowser.open(f"{cm}")
              
 #Open Facebook
-         elif "Open Facebook" in query:
-             webbrowser.open("Facebook.com")
+        elif "open facebook" in query:
+            webbrowser.open("Facebook.com")
 
 #Open Stack Flow
-         elif "Open Stackoverflow" in query:
-             webbrowser.open("Stackoverflow.com")
+        elif "open stackoverflow" in query:
+            webbrowser.open("Stackoverflow.com")
 
 #Open Google 
-         elif"Open Google" in query:
-             speak ("Sir, What should i search on Google ?")
-             cm=takecommand().lower()
-             webbrowser.open(f"{cm}")
+        elif"open google" in query:
+            speak ("Sir, What should i search on Google ?")
+            cm=takecommand().lower()
+            webbrowser.open(f"{cm}")
 
 #Open Instagram
-         elif "Open Instagram" in query:
-             webbrowser.open("instagram.com/tanay_30104")
+        elif "open instagram" in query:
+            webbrowser.open("instagram.com/tanay_30104")
 
 #Email
-         elif "Send Email" is query:
-             try:
-                 speak("What should i say?")
-                 content=takecommand().lower()
-                 to = "programmingforjuniors@gmail.com" 
-                 sendEmail(to,content)
-                 speak ("Email had been send successfully !")
+        elif "send email" in query:
+            try:
+                speak("What should i say?")
+                content=takecommand().lower()
+                to = "programmingforjuniors@gmail.com" 
+                sendEmail(to,content)
+                speak ("Email had been send successfully !")
 
             except Exception as e:
                 print (e)
                 speak ("Sorry Sir , i am not able to sent this email .")
 
 #Closing Loop 
-             elif "No Thanks" in query :
-                 speak ("Thanks for using me . Have a good Day!")
-                 sys.exit()
+        elif "thank you " in query :
+            speak ("Thanks for using me . Have a good Day!")
+            sys.exit()
 
-             speak ("Sir, Do You have any other work ?")
+            speak ("Sir, Do You have any other work ?")
 
 
